8e874637b422410c9e1e00a5a35a22d66d6ce4cf
TomerBu <TomerBu@gmail.com>
2025-02-06 14:40:07 +0200

    lec2

--- Python/Lec2/6_practice.py
+++ Python/Lec2/6_practice.py
@@ -1,11 +1,30 @@
 from random import choice
+from hangman import HANGMANPICS
 
-fruits = ["apple", "banana", "cherry", "kiwi", "mango", "orange", "pear", "pineapple", "strawberry", "watermelon"]
+lives = len(HANGMANPICS) - 1
+word_list = ["dog", "cat", "elephant", "home"]
 
-# random fruit:
-random_word = choice(fruits)
+chosen_word = choice(word_list)
 
+#testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
-apples = list("🍏"*len(random_word))
-print(apples) #['🍏', '🍏', '🍏', '🍏', '🍏', '🍏', '🍏', '🍏', '🍏']
+display = ["_" for _ in chosen_word]
 
+while "_" in display and lives > 0:
+    print(display)
+    print(HANGMANPICS[lives])
+    guess = input("Guess a letter: ").lower()
+
+    if guess not in chosen_word:
+        lives -= 1
+    else:
+        # update the display
+        for i in range(len(chosen_word)):
+            if guess == chosen_word[i]:
+                display[i] = guess
+
+if lives > 0:
+    print("You win!")
+else:
+    print("You lose!")
