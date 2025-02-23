17a3122cec29acf5c79b0ee0b4a8b85bd5ca5adb
TomerBu <TomerBu@gmail.com>
2025-02-23 15:58:55 +0200

    replies must be on the same post id

--- Django/lec4/blog/views.py
+++ Django/lec4/blog/views.py
@@ -1,18 +1,65 @@
 from .serializers import *
-from .models import Comment, Post, UserProfile,PostUserLikes
-from rest_framework.viewsets import  ModelViewSet
- 
+from .models import Comment, Post, UserProfile, PostUserLikes
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+
+
 class CommentsViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        reply_to = data.get('reply_to')
+        post_id = data.get('post')
+        
+        if reply_to:
+            replied = Comment.objects.get(id=reply_to)
+            print(replied)
+            if (
+                replied and replied.post.id != post_id
+            ):
+                return Response(
+                    {"error": "Reply must be on the same post"},
+                    status=400
+                )
+
+        return super().create(request, *args, **kwargs)
+
+    # build a tree structure of comments
+
+    def list(self, request, *args, **kwargs):
+        """Builds a nested comment structure"""
+        res = super().list(request, *args, **kwargs)
+
+        comments = res.data
+        comments_dict = {comment["id"]: comment for comment in comments}
+        root_comments = []
+
+        for comment in comments:
+            parent_id = comment['reply_to']
+            if parent_id is None:
+                root_comments.append(comment)
+            else:
+                parent = comments_dict.get(parent_id)
+                if parent:
+                    if "replies" not in parent:
+                        parent["replies"] = []
+                    parent["replies"].append(comment)
+
+        res.data = root_comments
+        return res
+
 
 class PostsViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+
 class UserProfileViewSet(ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+
 
 class LikesViewSet(ModelViewSet):
     queryset = PostUserLikes.objects.all()
@@ -58,11 +105,11 @@ class LikesViewSet(ModelViewSet):
 #     def get(self, request):
 #         # use ListModelMixin's list method
 #         return self.list(request)
-    
+
 #     def post(self, request):
 #         # use CreateModelMixin's create method
 #         return self.create(request)
-    
+
 # class PostActions(GenericAPIView, UpdateModelMixin, DestroyModelMixin, RetrieveModelMixin):
 #     queryset = Post.objects.all()
 #     serializer_class = PostSerializer
